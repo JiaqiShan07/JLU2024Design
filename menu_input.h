397dc0808bc30666f9345e67484f95971581090c
Isidore <isidore0126@gmali.com>
2025-03-28 22:22:20 +0800

    032809_1

--- menu_input.h
+++ menu_input.h
@@ -20,6 +20,7 @@ int handleShelfMenuInput(PackageSystem* system,
                          ShelfSystem* shelf_system);
 // 处理管理员日志菜单的输入
 int handleAdminLogMenuInput(UserSystem* user_system, PackageSystem* system);
+// 处理二级通知菜单的输入
 int handlesecondNotificationMenuInput(UserSystem* user_system,
                                       PackageSystem* package_system);
 #endif  // INPUT_MANAGEMENT_H
--- notification_handler.h
+++ notification_handler.h
@@ -4,6 +4,7 @@
 // 显示当前用户未读通知的数量，包括包裹状态变更等通知
 void showNotificationCount(UserSystem* user_system,
                            PackageSystem* package_system);
+// 展示各类包裹的通知详情
 void showNotificationPENDING_PICKUPDetails(UserSystem* user_system,
                                            PackageSystem* package_system);
 void showNotificationPENDING_DELIVERYDetails(UserSystem* user_system,
